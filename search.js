6ae3e55cba3d2462d8f1f2aa6393d8ba7367ae93
Kevin McGrath <kpmcgra@gmail.com>
2023-06-09 12:50:51 -0500

    naive string search

--- search.js
+++ search.js
@@ -16,3 +16,34 @@ function binarySearch(array, value){
     }
     return -1;
 }
+
+// conditional return statement alternate syntax.
+// return boolean ? value : (else) altvalue;
+
+//naive string search:
+//window search...move window and check for each number in window if it equals test string.
+function naiveStringSearch(test, string){
+    let window = test.length;
+    let stringCount = 0;
+    for(i = 0; i <= string.length - window ; i++){
+        let slicedBit = string.slice(i, i+window);
+        for(character in slicedBit){
+            //if if never fails the check needs to increase count....
+            if(slicedBit[character]!=test[character]){
+                break;
+            }
+            else if(character == window-1){
+                stringCount += 1;
+            }
+        }
+        
+    }
+    return stringCount;
+}
+
+console.log(naiveStringSearch("goat","goatgoatPolegoat"));
+console.log(naiveStringSearch("wart", "allthebestwartsaregreen"));
+
+
+
+
